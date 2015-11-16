6089a09206b2c8a5c97c936f73b31c513fb03839
Brendan Batliner <brendan.batliner@gmail.com>
2015-11-16 01:21:13 -0600

    Source code generated with templates

--- src/generator.js
+++ src/generator.js
@@ -1,63 +1,22 @@
 'use strict';
 
 var fs = require('fs');
+var path = require('path');
+var Mustache = require('mustache');
 var packager = require('electron-packager');
 
-var defaultDir = 'tmp_app_dir';
+var templatesDir = 'templates';
 var srcName = 'index.js';
 
 module.exports = function (options) {
-  var dir = options.dir || defaultDir;
+  var dir = options.dir || 'tmp_app_dir';
   var srcPath = dir + '/' + srcName;
   var pkgjsonPath = dir + '/package.json';
 
-  // Use a template string for the src file.
-  // TODO: Move this to another file for maintainability.
-  var src = `'use strict';
-
-var app = require('app');  // Module to control application life.
-var BrowserWindow = require('browser-window');  // Module to create native browser window.
-
-// Report crashes to our server.
-require('crash-reporter').start();
-
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-var mainWindow = null;
-
-// Quit when all windows are closed.
-app.on('window-all-closed', function() {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform != 'darwin') {
-    app.quit();
-  }
-});
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-app.on('ready', function() {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600, 'auto-hide-menu-bar': true, 'web-preferences': { 'node-integration': false }});
-
-  // and load the index.html of the app.
-  mainWindow.loadUrl('${options.url}');
-
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function() {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null;
-  });
-});
-  `;
+  var src = Mustache.render(fs.readFileSync(path.join(__dirname, templatesDir, 'index.mst'), 'utf8'), { url: options.url });
 
   // Generate the simplest package.json possible that tells Electron where to find the app.
-  var pkgjson = `{
-    "main": "${srcName}"
-  }
-  `;
+  var pkgjson = Mustache.render(fs.readFileSync(path.join(__dirname, templatesDir, 'package.json.mst'), 'utf8'), { srcName: srcName });
 
   // Write the src and package.json to a temp dir
   if (!fs.existsSync(dir)) {
